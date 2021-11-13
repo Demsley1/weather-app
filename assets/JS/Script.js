995704770015dc76397faa3c09cd1f6c2d89a143
Denva <emsley3684@gmail.com>
2021-11-12 20:07:57 -0500

    added functionality to retrieve input form value and use its value as a target in the api fetch request

--- assets/JS/Script.js
+++ assets/JS/Script.js
@@ -1,13 +1,59 @@
-var cityWeather = function() {
-    var now = dayjs();
-    console.log(now)
-}
-
-var getCityInfo = function() {
-    console.log("");
-}
 
 
+$(document).ready(function() {
+    // target user form and target form input value
+    var formEl = document.querySelector("#user-form")
+    var cityNameEl = document.querySelector("#cityVal")
+    var cityTitle = document.querySelector("#city-name")
 
-getCityInfo();
-cityWeather();
+    
+    // main function to fetch api, then parse rsponsei nto json to be be used and appended to the page
+    var getCityInfo = function(event) {
+        event.preventDefault();
+        var cityName = cityNameEl.value.trim();
+
+        if (cityName){
+            getCityData(cityName);
+            console.log(cityName);
+
+            cityNameEl.value = "";
+        }
+        else {
+            alert("Please enter a valid city name.")
+        }
+    }
+
+    var getCityData = function(city) {
+        var openWeather = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=1906d24e055219a4668f097f37c8b286"
+        console.log(openWeather);
+
+        fetch (openWeather).then(function(response){
+            response.json().then(function(data){
+                console.log(data)
+                if(response.ok){
+                    
+
+                }
+            });
+        });
+
+        var cityWeather = function() {
+            var day = dayjs();
+        cityTitle.textContent = "";
+        $( "#city-name" ).append(city + ':' + '('+day.$D+'/'+day.$M+'/'+day.$y+')');
+        }
+        cityWeather();
+    }
+
+
+    
+
+    formEl.addEventListener("submit", getCityInfo);
+   
+});
+
+
+
+
+
+
