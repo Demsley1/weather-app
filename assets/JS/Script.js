c07dea0c16cf4a4a363e5c9bb998e10f29947bcb
Denva <emsley3684@gmail.com>
2021-11-13 05:54:51 -0500

    finished displayCityTemp function

--- assets/JS/Script.js
+++ assets/JS/Script.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     // target user form and target form input value
     var formEl = document.querySelector("#user-form")
     var cityNameEl = document.querySelector("#cityVal")
+    // variable to target city name h2 element, should be removed for jQuery solutions
     var cityTitle = document.querySelector("#city-name")
 
     
@@ -25,13 +26,13 @@ $(document).ready(function() {
 
     var getCityData = function(city) {
         var openWeather = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=1906d24e055219a4668f097f37c8b286"
-        console.log(openWeather);
 
         fetch (openWeather).then(function(response){
-            response.json().then(function(data){
-                console.log(data)
+            response.json().then(function(data){ 
                 if(response.ok){
-                    
+                   oneCall(data)
+                }
+                else {
 
                 }
             });
@@ -40,13 +41,69 @@ $(document).ready(function() {
         var cityWeather = function() {
             var day = dayjs();
         cityTitle.textContent = "";
-        $( "#city-name" ).append(city + ':' + '('+day.$D+'/'+day.$M+'/'+day.$y+')');
+        $( "#city-name" ).append( city + ':' + '('+day.$D+'/'+day.$M+'/'+day.$y+')');
         }
         cityWeather();
     }
 
+    var oneCall = function(coords) {
+        var lon = coords.coord.lon
+        var lat = coords.coord.lat
+        var apiOneCall = ("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=imperial&appid=1906d24e055219a4668f097f37c8b286")
 
-    
+        fetch (apiOneCall).then(function(response){
+            response.json().then(function(data2){
+                console.log(data2);     
+                if(response.ok){
+                   displayCityTemp(data2)
+                }
+                else {
+
+                }
+            });
+        });
+        
+        var displayCityTemp = function(temp) {
+            // set each array value to a funciton
+            var tempOfCity = temp.current.temp;
+            var wind = temp.current.wind_speed;
+            var hum = temp.current.humidity;
+            var uvi = temp.current.uvi;
+
+            // container for each element
+            var tempEl = document.createElement("div")
+            tempEl.classList = "list-item flex-row justify-space-between align-center";
+            var windEl=document.createElement("div")
+            windEl.classList = "list-item flex-row justify-space-between align-center";
+            var humEl = document.createElement("div")
+            humEl.classList = "list-item flex-row justify-space-between align-center";
+            var uviEl = document.createElement("div")
+            uviEl.classList = "list-item flex-row justify-space-between align-center";
+            // Name the title for each div and parse value attributed to title
+            var tempTitle = document.createElement("span")
+            tempTitle.textContent = ("Temperature:" + tempOfCity + "F");
+            var windTitle = document.createElement("span")
+            windTitle.textContent = ("Wind Speed:"+ wind + "MPH");
+            var humTitle = document.createElement("span")
+            humTitle.textContent = ("Humidity:" + hum + "%");
+            var uviTitle = document.createElement("span")
+            uviTitle.textContent = ("UV Index:" + uvi);
+
+            // Append each div to container
+            $(tempEl).append(tempTitle);
+            $(windEl).append(windTitle);
+            $(humEl).append(humTitle);
+            $(uviEl).append(uviTitle);
+            
+            $("#city-temp").append(tempEl)
+            $("#city-temp").append(windEl)
+            $("#city-temp").append(humEl)
+            $("#city-temp").append(uviEl)
+            
+
+        }
+
+    }
 
     formEl.addEventListener("submit", getCityInfo);
    
