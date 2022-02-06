4a0cf9fe4794604218b8ff7e1c8356b54679b9b0
Denva <emsley3684@gmail.com>
2022-02-06 09:54:10 -0500

    added comments to page

--- assets/JS/Script.js
+++ assets/JS/Script.js
@@ -134,43 +134,51 @@
             $("#city-temp").text(" ")
             $("#city-temp").append(listCon)
 
-
-            
-            
+            // function to handle the display of the 5-day daily forecast weather cards
             var dailyForecast = function(temp) {
+                // clears all fore-cast cards that might have been on the page already
                 $("#forecast-cards").text(" ")
                 for(let i = 1; i < 6; i++){
-                    var dailyTemp = temp.daily[i];
+                    // create a daily temp object with the data for each daily temp fetched from the api call
+                    const dailyTemp = temp.daily[i];
                     var dDate = dailyTemp.dt
                     var dTemp = dailyTemp.temp.day
                     var dWind = dailyTemp.wind_speed
                     var dHum = dailyTemp.humidity
                     var dIcon = dailyTemp.weather[0].icon
                     var tempPic = ("http://openweathermap.org/img/wn/"+dIcon+"@2x.png")
+                    // re-formated date for daily weather using day.js
                     var showDate = dayjs.unix(dDate).format('ddd, MMM DD YYYY');
 
+                    // create a card to hold the daily forecast
                     var forecastEl = document.createElement("div")
                     forecastEl.classList = "card dwcard m-2";
 
+                    // title of daily weather cards, as the formatted date
                     var forecastTitle = document.createElement('h3')
                     forecastTitle.textContent = (showDate);
                     forecastTitle.classList = "card-title text-white m-1";
 
+                    // image of the daily weather
                     var iconEl = document.createElement("img")
                     $(iconEl).attr("src", tempPic)
                     iconEl.classList = "card-img-top w-50"
 
+                    // container to hold all body elements of daily weather cards
                     var forecastBody = document.createElement('div')
                     forecastBody.classList = "card-body fs-4";
 
+                    // text element displaying temperature in farenheight
                     var forecastTemp = document.createElement('p')
                     forecastTemp.textContent = ("Temp:  " + dTemp + " °F")
                     forecastTemp.classList= "card-text text-li";
 
+                    // text element displaying wind in miles per hour
                     var forecastWind = document.createElement('p')
                     forecastWind.textContent = ("Wind:  " + dWind + "  MPH")
                     forecastWind.classList = "card-text text-li";
 
+                    // text element displaying the percentage of humidity
                     var forecastHum = document.createElement('p')
                     forecastHum.textContent = ("Humidity:  " + dHum + " %")
                     forecastHum.classList = "card-text text-li";
@@ -191,17 +199,19 @@
                 }
 
             }
-
             dailyForecast(temp);
             
         }  
     }
-
-        var saveSearch = (city) => {
+        // function to save search value to local storage
+        const saveSearch = (city) => {
+            // get local storage data if exists, and add new object with values to it then send it back to the server as json data
             var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+            // if there is no local storage data for this app create a new array to hold the data
             if(!searchHistory){
                 searchHistory = []
             } 
+            // if the local storage data doesn't hold the value being added to the array yet, then you can push the city name into the array
             if (searchHistory.indexOf(city) === -1){
                 searchHistory.push(city)
             }
@@ -209,8 +219,7 @@
             showSearch();
         }
 
-        
-
+        // function to get and display each local storage item as clickable button 
         const showSearch = () => {
             searchList.innerHTML = " ";
             var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
@@ -222,6 +231,7 @@
             })
         }
 
+        // when each search button is clicked rerun the search for that city
         function searchBtn(event) {
             event.preventDefault();
             getCityData(event.target.textContent)
